c0a385947ffa96a150b556b7fd9a426c2d5550fc
avallonsmith <avallonsmith@gmail.com>
2022-04-06 01:03:00 +0000

    incomplete

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 /* global data */
 /* exported data */
+//* function photoInput(event) {
+//*  console.log('event:', event);
+//* }
+//* var $photoURL = document.querySelector('.photoUrlBox');
+//* $photoURL.addEventListener('input:', photoInput);
+
+//* var $submit = document.querySelector();
+
+//* function submitFunction(event)
+//* event.preventDefault();
+//*   var title = $submit.elements.title.value;
+//*   var photoUrl = $submit.elements.photoUrl.value;
+//*   var notes = $submit.elements.notes.value;
+//*  var formObjects = {
+//*    title: title,
+//*    url: photoUrl,
+//*    notes: notes,
+//*    id: data.nextEntryId
+//* };
+//* $submit.addEventListener('submit:', $submit);
