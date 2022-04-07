73c76d1c2d8da29327897cd1c47241e38cc4670b
avallonsmith <avallonsmith@gmail.com>
2022-04-07 00:40:25 +0000

    completed part one: user-can-create-an-entry

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var entriesJSON = localStorage.getItem('javascript-local-storage');
+if (entriesJSON !== null) {
+  data = JSON.parse(entriesJSON);
+}
+function stringy(event) {
+  var dataArray = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataArray);
+}
+window.addEventListener('beforeunload', stringy);
--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
 /* global data */
 /* exported data */
-//* function photoInput(event) {
-//*  console.log('event:', event);
-//* }
-//* var $photoURL = document.querySelector('.photoUrlBox');
-//* $photoURL.addEventListener('input:', photoInput);
+function photoInput(event) {
+  if (event.target.name === 'photoUrl') {
+    $imgPreview.setAttribute('src', event.target.value);
+  }
+}
+var $photoURL = document.querySelector('.photoUrlBox');
+$photoURL.addEventListener('input', photoInput);
 
-//* var $submit = document.querySelector();
+var $imgPreview = document.querySelector('#img-preview');
 
-//* function submitFunction(event)
-//* event.preventDefault();
-//*   var title = $submit.elements.title.value;
-//*   var photoUrl = $submit.elements.photoUrl.value;
-//*   var notes = $submit.elements.notes.value;
-//*  var formObjects = {
-//*    title: title,
-//*    url: photoUrl,
-//*    notes: notes,
-//*    id: data.nextEntryId
-//* };
-//* $submit.addEventListener('submit:', $submit);
+var $submit = document.querySelector('#entry-form');
+function submitFunction(event) {
+  event.preventDefault();
+  var title = $submit.elements.title.value;
+  var photoUrl = $submit.elements.photoUrl.value;
+  var notes = $submit.elements.notes.value;
+  var formObjects = {
+    title: title,
+    url: photoUrl,
+    notes: notes,
+    id: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.unshift(formObjects);
+  $submit.reset();
+  $imgPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
+}
+
+$submit.addEventListener('submit', submitFunction);
